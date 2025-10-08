cf9a037ea29e98bb9b06c991a6cbd922756a9d53
Magnus Markling <magnus@markling.com>
2025-10-08 11:18:11 +0200

    read and print target quadrant

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-fn main() {
-    // Statements here are executed when the compiled binary is called.
+#![allow(unused_variables)]
 
-    // Print text to the console.
-    println!("Hello World!");
+use anyhow::Result;
+
+fn main() {
+    let q = get_target_quadrant().unwrap();
+
+    println!("{q}");
+}
+
+fn get_target_quadrant() -> Result<u8> {
+    Ok(ureq::get("http://192.168.0.177:31415/quadrant")
+        .header("Authorization", "606545")
+        .call()?
+        .body_mut()
+        .read_to_string()?
+        .parse()?)
 }
