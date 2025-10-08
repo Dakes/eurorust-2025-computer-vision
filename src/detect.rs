ab9cb1933a821757f7bd29812f826b58cfcc1d42
David Scherle <d.scherle@meteocontrol.de>
2025-10-08 12:12:43 +0200

    8 param fucntion

--- src/detect.rs
+++ src/detect.rs
@@ -17,11 +17,14 @@ pub fn detect_markers(img: &Mat) -> opencv::Result<Detection> {
     )?;
     let params = aruco::DetectorParameters::create()?;
 
+    let camera_matrix = Mat::default();
+    let dist_coeffs = Mat::default();
+
     let mut corners = Vector::new();
     let mut ids_mat = Mat::default();
     let mut rejected = Vector::new();
 
-    aruco::detect_markers(img, &dict, &mut corners, &mut ids_mat, &params, &mut rejected)?;
+    aruco::detect_markers(img, &dict, &mut corners, &mut ids_mat, &params, &mut rejected, &camera_matrix, &dist_coeffs)?;
 
     let ids = mat_ids_to_vec(&ids_mat)?;
     Ok(Detection { ids, corners, rejected })
