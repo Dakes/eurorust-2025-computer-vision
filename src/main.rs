fd9a377d754a0c1a658e072f54d9ff4dcca9c740
Magnus Markling <magnus@markling.com>
2025-10-08 11:32:06 +0200

    retrieve camera vectors

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use anyhow::Result;
 
 fn main() {
     let q = get_target_quadrant().unwrap();
-
     println!("{q}");
+
+    let i1 = get_camera_1_vec().unwrap();
+    println!("{:?}", &i1[..10]);
+
+    let i2 = get_camera_2_vec().unwrap();
+    println!("{:?}", &i2[..10]);
 }
 
 fn get_target_quadrant() -> Result<u8> {
@@ -15,4 +20,20 @@ fn get_target_quadrant() -> Result<u8> {
         .body_mut()
         .read_to_string()?
         .parse()?)
+}
+
+fn get_camera_1_vec() -> Result<Vec<u8>> {
+    Ok(ureq::get("http://hackathon-11-camera.local:50051/frame")
+        .header("Authorization", "983149")
+        .call()?
+        .body_mut()
+        .read_to_vec()?)
+}
+
+fn get_camera_2_vec() -> Result<Vec<u8>> {
+    Ok(ureq::get("http://hackathon-12-camera.local:50051/frame")
+        .header("Authorization", "378031")
+        .call()?
+        .body_mut()
+        .read_to_vec()?)
 }
